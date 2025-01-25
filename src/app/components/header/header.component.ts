b4fa9c963cbeaeb4b9dbf6717c596919e2ba595a
Luiz <luizinhon@hotmail.com>
2025-01-25 19:00:38 -0300

    feat: header component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component, inject } from '@angular/core';
+import { ThemeService } from '../../services/theme.service';
+import { ThemeSwitchComponent } from '../theme-switch/theme-switch.component';
+
+@Component({
+  selector: 'app-header',
+  imports: [ThemeSwitchComponent],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {
+  themeService: ThemeService = inject(ThemeService);
+}
