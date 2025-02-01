886d2ba1509d4863d339696b0fc0790f1d757370
Luiz <luizinhon@hotmail.com>
2025-02-01 01:19:23 -0300

    test: services and pipes

--- src/app/pipes/task-status-filter.pipe.spec.ts
+++ src/app/pipes/task-status-filter.pipe.spec.ts
@@ -1,8 +1,49 @@
+import { TaskStatus } from '../enums/task-status.enum';
 import { TaskStatusFilterPipe } from './task-status-filter.pipe';
+
+const tasks = [
+  {
+    id: 'task_1',
+    name: 'Example 1',
+    done: false,
+  },
+  {
+    id: 'task_2',
+    name: 'Example 2',
+    done: false,
+  },
+  {
+    id: 'task_3',
+    name: 'Example 3',
+    done: true,
+  },
+];
 
 describe('TaskStatusFilterPipe', () => {
   it('create an instance', () => {
     const pipe = new TaskStatusFilterPipe();
+
     expect(pipe).toBeTruthy();
+  });
+
+  it('should filter all tasks', () => {
+    const pipe = new TaskStatusFilterPipe();
+    const filteredTasks = pipe.transform(tasks, TaskStatus.ALL);
+
+    expect(filteredTasks).toHaveLength(3);
+  });
+
+  it('should filter todo tasks', () => {
+    const pipe = new TaskStatusFilterPipe();
+    const filteredTasks = pipe.transform(tasks, TaskStatus.TODO);
+
+    expect(filteredTasks).toHaveLength(2);
+  });
+
+  it('should filter done tasks', () => {
+    const pipe = new TaskStatusFilterPipe();
+    const filteredTasks = pipe.transform(tasks, TaskStatus.DONE);
+
+    expect(filteredTasks).toHaveLength(1);
   });
 });
--- src/app/services/theme.service.spec.ts
+++ src/app/services/theme.service.spec.ts
@@ -1,9 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ThemeService } from './theme.service';
 
 describe('ThemeService', () => {
   let service: ThemeService;
+  localStorage.setItem('theme', 'dark');
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -12,5 +12,21 @@ describe('ThemeService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should set theme', () => {
+    service.setTheme('light');
+
+    expect(service.themeSignal()).toEqual('light');
+  });
+
+  it('should update theme', () => {
+    service.updateTheme();
+
+    expect(service.themeSignal()).toEqual('light');
+  });
+
+  it('should get saved theme', () => {
+    expect(service.themeSignal()).toEqual('dark');
   });
 });
